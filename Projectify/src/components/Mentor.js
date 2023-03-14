03a468c3c2933fa919fd985e70c6eadeaec3b972
Ojasvi01 <97157187+Ojasvi01@users.noreply.github.com>
2023-03-15 00:29:06 +0530

    Mentor and Project card
    Created cards to display the projects and mentors on the website

---
+++ Projectify/src/components/Mentor.js
@@ -0,0 +1,80 @@
+import React from "react";
+import './App.css';
+function Mentors(){
+  return (
+    <div className="body">
+<div className="card">
+    <h3 className="title">Mentor 1 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 2 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 3 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 4 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 5 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 6 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 7 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 8 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 9 Name</h3>
+    <span className="ID">Mentor Id</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+<div className="card">
+    <h3 className="title">Mentor 10 Name</h3>
+    <span className="ID">Work Experince</span>
+    <div className="Mentor">Email Add</div>
+    <div className="domain">Domain Name</div>
+    
+</div>
+
+</div>
+  );
+}
+export default Mentors;

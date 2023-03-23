7511016c2de8ecad026ee3e866f5d641aa3301bc
Samriddhi Singh <108535666+Samriddhii-Siingh@users.noreply.github.com>
2023-03-23 20:27:49 +0530

    Added routes for footer

--- Projectify/src/App.js
+++ Projectify/src/App.js
@@ -1,11 +1,17 @@
 import "./styles.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./components/pages/Home";
 import Project from "./components/pages/Project";
 import Form from "./components/pages/Form";
 import Dashboard from "./components/pages/Dashboard";
 import Login from "./components/pages/Login";
+
+import AboutUs from "./components/pages/AboutUs";
+import ContactUs from "./components/pages/ContactUs";
+import Services from "./components/pages/Services";
+import PrivacyPolicy from "./components/pages/PrivacyPolicy";
 
 import { Route, Routes } from "react-router-dom";
 
@@ -20,7 +26,14 @@ export default function App() {
         <Route path="/form" element={<Form />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/services" element={<Services />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 }

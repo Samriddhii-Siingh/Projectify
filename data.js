c837a6dfa6395a5480f2b0b41a1aa4acd74fc38c
Ojasvi01 <97157187+Ojasvi01@users.noreply.github.com>
2023-03-26 23:08:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+const data=[
+    {
+        text:'Wrote my first blog post ever on Medium',
+        date:'March 03 2017',
+        category:{
+            tag:'medium',
+            color:'#018f69'
+        },
+        link:{
+            url:'google.com',
+            text:'Read more'
+        }
+    },
+    {
+    text:'Started working on the app ideas repo',
+    date:'February 25 2019',
+    category:{
+        tag:'add-ideas',
+        color:'#018f69'
+    },
+    link:{
+        url:'#',
+        text:'Check in out'
+    }
+    },
+    {
+        text:'Started the weekly coding challenge',
+        date:'March 04 2019',
+        category:{
+            tag:'blog',
+            color:'#e17b77'
+        },
+        link:{
+            url:'#',
+            text:'check it out'
+        }
+    },
+    {
+        text:'Over 12000 views in a single day on my post',
+        date:'April 05 2019',
+        category:{
+            tag:'medium',
+            color:'#018f69'
+        },
+        link:{
+            url:'#',
+            text:'See Profile'
+        }
+    },
+    {
+        text:'Wrote my first blog post ever on Medium',
+        date:'March 03 2017',
+        category:{
+            tag:'medium',
+            color:'#018f69'
+        },
+        link:{
+            url:'google.com',
+            text:'Read more'
+        }
+    },
+    {
+    text:'Started working on the app ideas repo',
+    date:'February 25 2019',
+    category:{
+        tag:'add-ideas',
+        color:'#018f69'
+    },
+    link:{
+        url:'#',
+        text:'Check in out'
+    }
+}
+]
+export default data;

36e292992cc01bb8e8d79eb098616782ad157d55
Samriddhi Singh <108535666+Samriddhii-Siingh@users.noreply.github.com>
2023-04-19 03:01:33 +0530

    Update in files

--- Projectify/src/components/pages/Home.js
+++ Projectify/src/components/pages/Home.js
@@ -1,3 +1,19 @@
+import React from 'react';
+import './Home.css';
+import homeimg from './assets/home.jpg';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+
+    <div className='home-container'>
+      <div className='home-image-container'>
+        <img src={homeimg} alt="home_image" />
+      </div>
+      <div className='home-text-container'>
+        <h1 className='home-headline'>PROJECTIFY</h1>
+        <p className='home-text-1'>The Ultimate Platform for Project-based Learning.</p>
+        <p className='home-text-2'>Choose Your Domain and Enhance Your Problem-Solving Skills with Variety of Projects along with Mentor-Guided Real-Life Application Development.</p>
+      </div>
+    </div>
+  );
 }
--- Projectify/src/components/pages/Login.js
+++ Projectify/src/components/pages/Login.js
@@ -1,50 +1,54 @@
-import React,{useState} from "react";
-import './Login.css';
-import logo from './assets/login2.gif';
-function Login(){
-    const[email,setEmail]=useState("");
-    const[password,setPassword]=useState("");
-    const handleGoogleLogin=()=>{
-    };
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        console.log(email);
-    };
-    const handleForgetPassword=()=>{
-    };
-    return(
-        <>
-        <div>
-            <img src={logo} alt="loading"/>
-        </div>
-        <div className="card">
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "./assets/login2.gif";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleGoogleLogin = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+  const handleForgetPassword = () => {};
+  return (
+    <>
+      <div>
+        <img src={logo} alt="loading" />
+      </div>
+      <div className="card">
         <h1>Login HERE</h1>
         <form className="form" onSubmit={handleSubmit}>
-            <input
+          <input
             className="email"
             type="email"
             id="email"
             placeholder="UserName"
             value={email}
-            onChange={(e)=>setEmail(e.target.value)}
-            />
-            <input
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
             className="password"
             type="password"
             id="password"
             placeholder="Password"
             value={password}
-            onChange={(e)=>setPassword(e.target.value)}
-            />
-        <button className="submit-button" type="submit">Sign-In</button>
-        <button className="google-button" onClick={handleGoogleLogin}>Sign In with Google
-        <span className="google-logo"></span></button>
-        <p className="forgot-password">
-            <a href="/#" onClick={handleForgetPassword}>New User?</a>
-        </p>
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="submit-button" type="submit">
+            Sign-In
+          </button>
+          <button className="google-button" onClick={handleGoogleLogin}>
+            Sign In with Google
+            <span className="google-logo"></span>
+          </button>
+          <p className="forgot-password">
+            <a href="/#" onClick={handleForgetPassword}>
+              New User?
+            </a>
+          </p>
         </form>
-        </div>
-        </>
-    );
-    }    
+      </div>
+    </>
+  );
+}
 export default Login;
